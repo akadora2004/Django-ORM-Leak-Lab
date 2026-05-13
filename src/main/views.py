03f75363b2b87032d5bd405896464291af7e66b8
akadora2004 <akadora2004@gmail.com>
2026-05-14 00:25:57 +0900

    Remove department filter from safe view
    Removed department filter from safe view.

--- src/main/views.py
+++ src/main/views.py
@@ -16,13 +16,10 @@ def index(request):
 
 def safe(request):
     name = request.GET.get('name')
-    dept = request.GET.get('department')
 
     results2 = Employee.objects.all()
     if name:
         results2 = results2.filter(name=name)
-    if dept:
-        results2 = results2.filter(department=dept)
 
     return render(request, 'main/safe.html', {
         'results2': results2,
